95bdf3ac37aa428fc3875fc518d23f2a2874de1a
Bodjanja <bodjanja2210@gmail.com>
2022-06-20 13:56:21 +0300

    corr

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const { PORT = 3000, MONGO_DB_ADRESS = 'mongodb://localhost:27017/moviesdb', JWT_SECRET = 'JWT_SECRET' } = process.env;
+const { PORT = 3001, MONGO_DB_ADRESS = 'mongodb://localhost:27017/moviesdb', JWT_SECRET = 'JWT_SECRET' } = process.env;
 module.exports = {
   PORT,
   MONGO_DB_ADRESS,
